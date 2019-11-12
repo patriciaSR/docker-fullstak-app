87a8af7a23db7b32ed6de0efe6a9f074b18f8c19
patriciaSR <patricia.suarez.psr@gmail.com>
2019-11-12 21:45:49 +0100

    first commit

---
+++ api/index.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+const mongodb = require('mongodb');
+const DB = {
+	config: 'mongodb://mongo:27017'
+};
+let dbo;
+
+app.get('/', function (req, res) {
+	res.json({ "hello": "express with mongo" });
+});
+
+const client = mongodb.MongoClient;
+client.connect(DB.config, function (err, db) {
+	if (err) {
+		console.log('database is not connected')
+	}
+	else {
+		console.log('connected!!');
+		dbo = db.db("midb");
+	}
+});
+app.get('/misdatos', function (req, res) {
+	let data = dbo.collection("micoleccion").find({}).toArray((err, result) => {
+		if (err) throw err;
+		res.json(result);
+	});
+});
+app.listen(PORT, function () {
+	console.log('Your node js server is running on PORT:', PORT);
+});
