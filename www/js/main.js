73052a4cb5b40cb62babf8eb5a9cfb3ae3232412
Clara Dios <clara.dios.diez@gmail.com>
2019-11-13 12:20:55 +0100

    listen to every task and delete when pressing delete button

--- www/js/main.js
+++ www/js/main.js
@@ -2,9 +2,34 @@
 
 const input = document.querySelector('.create__field');
 const btn = document.querySelector('.create__btn');
+const list = document.querySelector('.list');
+
 
 function createTask () {
-    console.log('crea tarea');
+    
+    const newItem = document.createElement('li');
+    const task = document.createTextNode(input.value);
+    
+    const newCheckbox = document.createElement('input');
+    newCheckbox.type = 'checkbox';
+
+    const newDelBtn = document.createElement('button');
+    const btnText = document.createTextNode('delete');
+   
+    newDelBtn.appendChild(btnText);
+    newItem.appendChild(newCheckbox);
+    newItem.appendChild(task);   
+    newItem.appendChild(newDelBtn);
+    list.appendChild(newItem);
+     
+    newDelBtn.addEventListener('click', deleteTask);
+    // newCheckbox.addEventListener('click', changeStatus);
+}
+
+function deleteTask(event) {
+    const currentBtn = event.currentTarget;
+    const liItem = currentBtn.parentElement;
+    liItem.remove();
 }
 
 btn.addEventListener('click',createTask);
