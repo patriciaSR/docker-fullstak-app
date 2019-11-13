3b642addade3ae5d394f1caaf6a3232751cb3d1c
Clara Dios <clara.dios.diez@gmail.com>
2019-11-13 11:45:39 +0100

    listen to create button

--- www/js/main.js
+++ www/js/main.js
@@ -3,3 +3,8 @@
 const input = document.querySelector('.create__field');
 const btn = document.querySelector('.create__btn');
 
+function createTask () {
+    console.log('crea tarea');
+}
+
+btn.addEventListener('click',createTask);
