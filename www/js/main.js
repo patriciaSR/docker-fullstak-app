be200f48ce79a663066539b370ca71f1393994ee
patriciaSR <patricia.suarez.psr@gmail.com>
2019-11-14 14:32:05 +0100

    create createTag and createNodeAdopt function

--- www/js/main.js
+++ www/js/main.js
@@ -8,6 +8,7 @@ const infoText = document.querySelector('.list__info');
 const ENDPOINT = 'http://localhost/api/misdatos';
 
 const noTaskMsg = 'no hay tareas';
+const taskMsg = 'aquí están tus tareas:';
 const noTaskInputMsg = 'Por favor, introduce una tarea';
 
 
@@ -44,28 +45,38 @@ function createListElements({ _id, task, checked }) {
     const newItem = document.createElement('li');
     newItem.classList.add('list__item')
     newItem.id = _id;
-    const taskT = document.createTextNode(task);
 
-    const newCheckbox = document.createElement('input');
-    newCheckbox.type = 'checkbox';
-    const newText = document.createElement('p');
-
-    const newDelBtn = document.createElement('button');
-    const btnText = document.createTextNode('-');
-
-    newText.appendChild(taskT);
-    newDelBtn.appendChild(btnText);
-    newItem.appendChild(newCheckbox);
-    newItem.appendChild(newText);
-    newItem.appendChild(newDelBtn);
-    list.appendChild(newItem);
-
-    newDelBtn.addEventListener('click', deleteTask);
-    newCheckbox.addEventListener('click', changeStatus);
+    const newCheckbox = createTag('input', null, null, 'checkbox');
+    const newText = createTag('p', task);
+    const newDelBtn = createTag('button', '-');
 
     isCheked(newItem, newCheckbox, checked);
+    
+    createNodeAdopt(newItem, newCheckbox, newText, newDelBtn);
+    list.appendChild(newItem);
+    
+    newDelBtn.addEventListener('click', deleteTask);
+    newCheckbox.addEventListener('click', changeStatus);
+    
+    updateMsg(taskMsg);
+}
 
-    updateMsg('aquí están tus tareas:');
+function createTag(tag, text, newClass, newType) {
+    const newElement = document.createElement(tag);
+    const newText = document.createTextNode(text);
+    
+    newClass ? newElement.classList.add(newClass): null;
+    newType ? (newElement.type = newType) : null;
+    
+    newElement.appendChild(newText);
+
+    return newElement;
+}
+
+function createNodeAdopt(mother, ...rest) {
+    const newMother = rest.map(node => mother.appendChild(node));
+    
+    return newMother;
 }
 
 function isCheked(liItem, checkBox, status) {
